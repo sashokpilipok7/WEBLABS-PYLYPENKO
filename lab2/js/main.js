f7d4724fdcb0b5bd9c096739bd040a7343f5d024
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-04-02 11:05:22 +0300

    feat: add project for lab 2

---
+++ lab2/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger-menu");
+  const menu = document.querySelector(".header__nav");
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  burger.addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.classList.toggle("not-active");
+    menu.classList.toggle("header__nav_active");
+  });
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+
+      if (blockID === "contactUs") {
+        setTimeout(() => emailInput.focus(), 1500);
+      }
+    });
+  }
+});
